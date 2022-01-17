0790534e30e53bb7dcc0d1c2e5d677aaa72d35f7
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-17 16:13:22 +0100

    Delete correct_answer variable

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-correct_answers = 0
 window_title = f"0/50 States correct"
 guessed_states = []
 while len(guessed_states) < 50:
@@ -17,9 +16,8 @@ while len(guessed_states) < 50:
         window_title = f"Already on the map."
     elif state_obj.is_name_ok():
         state_obj.put_name_in_place()
-        correct_answers += 1
-        window_title = f"{correct_answers}/50 States correct"
         guessed_states.append(state_obj.state_name)
+        window_title = f"{len(guessed_states)}/50 States correct"
     else:
         window_title = f"Wrong! Try again."
 
