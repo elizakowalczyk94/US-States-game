c223bed40e3e334c403f31a741def5d82a934588
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-17 15:32:48 +0100

    Add while loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import state
 
 screen = turtle.Screen()
 screen.title("US States game")
@@ -6,13 +7,21 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-
-
-
-
- 
-
-
+is_game_on = True
+correct_answers = 0
+window_title = f"0/50 States correct"
+guessed_states = []
+while is_game_on:
+    user_guess = screen.textinput(title=window_title, prompt="What's another state's name?")
+    state_obj = state.State(user_guess)
+    if state_obj.state_name in guessed_states:
+        window_title = f"Already on the map."
+    elif state_obj.is_name_ok():
+        state_obj.put_name_in_place()
+        correct_answers += 1
+        window_title = f"{correct_answers}/50 States correct"
+        guessed_states.append(state_obj.state_name)
+    else:
+        window_title = f"Wrong! Try again."
 
 screen.exitonclick()
