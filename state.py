473638e1f39164ec91760df0e23448e563513298
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-17 15:32:15 +0100

    Add put_name_in_place function

--- state.py
+++ state.py
@@ -3,14 +3,14 @@ from turtle import Turtle
 
 CSV_NAME = "50_states.csv"
 ALIGNMENT = "center"
-FONT = ("Courier", 10, "normal")
+FONT = ("Courier", 8, "normal")
 
 
 class State(Turtle):
 
     def __init__(self, state_name):
         super().__init__()
-        self.state_name = state_name
+        self.state_name = state_name.lower()
         self.states_file = pandas.read_csv(CSV_NAME)
         self.penup()
         self.hideturtle()
@@ -18,24 +18,22 @@ class State(Turtle):
 
     def is_name_ok(self):
         states_list = self.states_file.state.to_list()
-        if self.state_name in states_list:
+        states_list_lower = [name.lower() for name in states_list]
+        if self.state_name in states_list_lower:
             return True
         else:
             return False
 
     def get_coordinates(self):
-        if self.is_name_ok():
-            state_cor = self.states_file[self.states_file.state == self.state_name]
-            x_cor = state_cor.x
-            y_cor = state_cor.y
-            return x_cor, y_cor
-        else:
-            return False
+        state_cor = self.states_file[self.states_file.state == self.state_name.title()]
+        x_cor = int(state_cor.x)
+        y_cor = int(state_cor.y)
+        return x_cor, y_cor
 
     def put_name_in_place(self):
         x, y = self.get_coordinates()
         self.goto(x, y)
-        self.write(arg=self.state_name,
+        self.write(arg=self.state_name.title(),
                    move=False,
                    align=ALIGNMENT,
                    font=FONT)
